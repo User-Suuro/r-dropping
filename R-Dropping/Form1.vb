b5faf733b0920c4c7e8ccbb4d68dba3117a9a9d0
User-Suuro <godwingalvez26@gmail.com>
2026-04-01 04:02:03 +0800

    added resx. and readme

--- R-Dropping/Form1.vb
+++ R-Dropping/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
 
+
+
+    End Sub
 End Class
---
+++ README.md.txt
@@ -0,0 +1,6 @@
+Visual Studio 2026 (latest)  
+
+Please restart the solution or Visual Studio if the MainForm (Form1) breaks.  
+
+This Project is for academic purpose only.   
+
